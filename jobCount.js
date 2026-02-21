3708ff6f154e3dd35c6f5b83ecd36d8e02787318
rakibul875 <rakibulislamashik78@gmail.com>
2026-02-21 22:38:35 +0600

    Complite toggling section

--- jobCount.js
+++ jobCount.js
@@ -1,10 +1,38 @@
+let interviewList=[];
+let rejectedList=[];
+
 let totalCount=document.getElementById('total')
 let interviewCount=document.getElementById('interview')
 let rejectedCount=document.getElementById('rejected')
 
 let totalJobs=document.getElementById("total-jobs")
-console.log(totalJobs.children.length)
+
+const mainSection=document.querySelector('main')
+console.log(mainSection)
+
+
 function totalJobsCircular(){
     totalCount.innerText=totalJobs.children.length
+    interviewCount.innerText=interviewList.length;
+    rejectedCount.innerText=rejectedList.length;
 }
 totalJobsCircular()
+
+//toggling section
+
+const allButton=document.getElementById('all-btn')
+const interviewButton=document.getElementById('interview-btn')
+const rejectedButton=document.getElementById('rejected-btn')
+function toggling(id){
+    allButton.classList.remove('bg-blue-700','text-white')
+    interviewButton.classList.remove('bg-blue-700','text-white')
+    rejectedButton.classList.remove('bg-blue-700','text-white')
+    
+    allButton.classList.add('bg-gray-200','text-black')
+    interviewButton.classList.add('bg-gray-200','text-black')
+    rejectedButton.classList.add('bg-gray-200','text-black')
+
+    const select=document.getElementById(id)
+    select.classList.remove('bg-gray-200','text-black')
+    select.classList.add('bg-blue-700','text-white')
+}
