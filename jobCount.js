612510ce9381597f5cfd6bb23f23d1bcefe565bb
rakibul875 <rakibulislamashik78@gmail.com>
2026-02-23 12:25:42 +0600

    complite rejected section

--- jobCount.js
+++ jobCount.js
@@ -1,5 +1,6 @@
 let interviewList = [];
 let rejectedList = [];
+let currentStatus="all-btn"
 
 let totalCount = document.getElementById('total')
 let interviewCount = document.getElementById('interview')
@@ -33,6 +34,7 @@ function toggling(id) {
     rejectedButton.classList.add('bg-gray-200', 'text-black')
 
     const select = document.getElementById(id)
+    currentStatus=id
     select.classList.remove('bg-gray-200', 'text-black')
     select.classList.add('bg-blue-700', 'text-white')
     if(id=='interview-btn'){
@@ -42,6 +44,11 @@ function toggling(id) {
     else if(id=="all-btn"){
         totalJobs.classList.remove('hidden')
         filterSection.classList.add('hidden')
+    }
+    else if(id=="rejected-btn"){
+        totalJobs.classList.add('hidden')
+        filterSection.classList.remove('hidden')
+        renderRejected()
     }
 }
 
@@ -67,8 +74,38 @@ mainSection.addEventListener('click', function (event) {
         if (!jobList) {
             interviewList.push(cardInfo)
         }
+        rejectedList = rejectedList.filter(item => item.titleName != cardInfo.titleName)
         totalJobsCircular()
         renderThriving()
+    }
+
+    else if (event.target.classList.contains("rejected-button")) {
+        const allParentNode = event.target.parentNode.parentNode
+        const titleName = allParentNode.querySelector('.name').innerText
+        const jobName = allParentNode.querySelector('.job-name').innerText
+        const celeryName = allParentNode.querySelector('.celery').innerText
+        const statusName = allParentNode.querySelector('.status').innerText
+        const workName = allParentNode.querySelector('.work').innerText
+        allParentNode.querySelector('.status').innerText='Rejected'
+
+        let cardInfo = {
+            titleName,
+            jobName,
+            celeryName,
+            statusName:"Rejected",
+            workName,
+        }
+
+        const jobList = rejectedList.find(item => item.titleName === cardInfo.titleName)
+        if (!jobList) {
+            rejectedList.push(cardInfo)
+        }
+        interviewList = interviewList.filter(item => item.titleName != cardInfo.titleName)
+        if(currentStatus=="interview-btn"){
+            renderThriving()
+        }
+        totalJobsCircular()
+        
     }
 })
 
@@ -95,8 +132,43 @@ function renderThriving() {
                             users worldwide.</p>
 
                     <div class="flex gap-4">
-                        <button class="btn btn-outline btn-success">interview</button>
-                        <button class="btn btn-outline btn-error">interview</button>
+                        <button class="interview-button btn btn-outline btn-success">interview</button>
+                        <button class="rejected-button btn btn-outline btn-error">Rejected</button>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <button class="btn"><i class="fa-solid fa-trash"></i></button>
+                </div>
+        `
+        filterSection.appendChild(div)
+    }
+}
+
+function renderRejected() {
+    filterSection.innerHTML = ``
+    for (let rejected of rejectedList) {
+        let div = document.createElement('div')
+        div.className = 'md:flex justify-between mt-5 bg-zinc-100 p-8'
+        div.innerHTML = `
+                <div class="space-y-4">
+                    <div class="">
+                        <p class="name text-[#002C5C] font-semibold text-4">${rejected.titleName}</p>
+                        <p class="job-name text-neutral/50">React Native Developer</p>
+                    </div>
+                    <p class="celery text-neutral/50">Remote
+                        •
+                        Full-time
+                        •
+                        $130,000 - $175,000</p>
+
+                        <p class="status btn bg-base-300 border-none">${rejected.statusName}</p>
+                        <p class="work text-neutral/50 mt-2">Build cross-platform mobile applications using React Native.
+                            Work on products used by millions of
+                            users worldwide.</p>
+
+                    <div class="flex gap-4">
+                        <button class="interview-button btn btn-outline btn-success">interview</button>
+                        <button class="rejected-button btn btn-outline btn-error">Rejected</button>
                     </div>
                 </div>
                 <div class="mt-4">
